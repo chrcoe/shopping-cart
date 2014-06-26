08a691f50be8362471b9ba1405faaba5437a7b61
jrenkel <jrenkel@gmail.com>
2014-06-25 20:30:27 -0400

    Policy XML/XSD Policy Object Graph and JAXB marshaller Policy XML JUnit test

--- src/test/XMLTest.java
+++ src/test/XMLTest.java
@@ -2,9 +2,16 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import policy.TransactionPolicy;
 
 public class XMLTest {
 
@@ -17,8 +24,14 @@ public class XMLTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws Exception {
+		InputStream is = this.getClass().getResourceAsStream("Policies.xml");
+		JAXBContext jaxbContext = JAXBContext.newInstance(TransactionPolicy.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		TransactionPolicy pol  = (TransactionPolicy) jaxbUnmarshaller.unmarshal(is);
+		assertEquals(pol.getTransaction().size(),2);
+		assertEquals(pol.getTransaction().get(0).getId(),"CheckOut");
+		assertEquals(pol.getTransaction().get(1).getId(),"AddToCart");
 	}
 
 }
