b8639cb22d1763b7dc1de63d1c765db740b25fc3
jrenkel <jeremy@renkelwerks.com>
2014-07-13 22:16:06 -0400

    action bean for user/profile

---
+++ src/action/ProfileActionBean.java
@@ -0,0 +1,20 @@
+package action;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+
+public class ProfileActionBean implements ActionBean {
+
+	@Override
+	public ActionBeanContext getContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setContext(ActionBeanContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
