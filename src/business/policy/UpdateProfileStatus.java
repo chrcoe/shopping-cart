3801ce2988463a08f27289b62d7f3a85b61929ae
jrenkel <jeremy@renkelwerks.com>
2014-07-20 23:18:48 -0400

    implement policy for updating premium status after checkout

--- src/business/policy/UpdateProfileStatus.java
+++ src/business/policy/UpdateProfileStatus.java
@@ -1,5 +1,10 @@
 package business.policy;
 
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+
+import policy.Parameter;
 import model.User;
 import business.Context;
 import business.exceptions.PolicyException;
@@ -8,15 +13,36 @@ import business.exceptions.UserNotRegisteredException;
 public class UpdateProfileStatus extends Policy {
 
 	@Override
-	public void preCheck(Context context) throws PolicyException {}
+	public void preCheck(Context context) throws PolicyException {
+	}
 
 	@Override
 	public void postCheck(Context context) throws PolicyException {
 		User u = (User) context.get(User.class);
-		// get user orders
-		// if user orders are > 5 and status is not premium
-		//		update status to premium
+		Boolean isPremium = false;
+		int minPremium = Integer.MAX_VALUE;
+		for (Parameter pa : super.getPolicyInfo().getParameter()) {
+			if ("MinOrders".equalsIgnoreCase(pa.getId())) {
+				minPremium = Integer.parseInt(pa.getValue());
+				break;
+			}
+		}
+		if (minPremium == Integer.MAX_VALUE) {
+			throw new PolicyException("Missing Policy Parameter [MinOrders]");
+		}
+		dao.OrderDAO o;
+		try {
+			o = new dao.OrderDAO();
+			isPremium = (o.getOrdersByUserID(u.getUserID()).size() >= minPremium);
+		} catch (NamingException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+		if (isPremium) {
+			// set premium user status
+			// new dao.UserDAO().getUserByUserID(u.getUserID()).set
+		}
 	}
 
 }
